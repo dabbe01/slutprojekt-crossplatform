dbfe9dc83ccc556a17b6a9238e6423c709c237e6
Dabbe01 <dabbe01@bredband.net>
2021-06-03 19:57:41 +0200

    task component

--- src/Screens/CreateTask.js
+++ src/Screens/CreateTask.js
@@ -1,11 +1,22 @@
-import React from 'react';
-import { Pressable, StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
+import React, { useState } from 'react';
+import { Pressable, StyleSheet, Text, View, Image, TouchableOpacity, Button, } from 'react-native';
 
 export default function ScreenA({ navigation }) {
 
     const onPressHandler = () => {
         navigation.navigate('Screen_A')
     }
+    const Meny = () => (
+        setShowMeny(true)
+
+    )
+    const Back = () => (
+        setShowMeny(false)
+
+    )
+    let [showMeny, setShowMeny] = useState(false)
+
+
     const AppButton = ({ title }) => (
         <TouchableOpacity onPress={onPressHandler}
             style={styles.appButtonContainer}>
@@ -15,64 +26,61 @@ export default function ScreenA({ navigation }) {
             <Text style={styles.appButtonText}>{title}</Text>
         </TouchableOpacity>
     );
-        const ProfileButton = ({ title }) => (
-            <TouchableOpacity onPress={onPressHandler}
-                style={styles.menybuttonContainer}>
+
+
+    const ProfileButton = ({ title }) => (
+        <TouchableOpacity onPress={onPressHandler}
+            style={styles.ProfileButtonContainer}>
             <Image
                 source={require('../assets/BuilderIcon.png')}
                 style={styles.menybuttonLogo}
             />
             <Text style={styles.menybuttonText}>{title}</Text>
         </TouchableOpacity>
-        
-        )
-        const LogoutButton = ({ title }) => (
-            <TouchableOpacity onPress={onPressHandler}
-                style={styles.menybuttonContainer}>
+
+    )
+
+    const LogoutButton = ({ title }) => (
+        <TouchableOpacity onPress={onPressHandler}
+            style={styles.LogoutButtonContainer}>
             <Image
                 source={require('../assets/logout.png')}
                 style={styles.menybuttonLogo}
             />
             <Text style={styles.menybuttonText}>{title}</Text>
         </TouchableOpacity>
-        
-        )
-    const Meny = () =>(
-        <View style={styles.meny}>
-            <View>
-            <ProfileButton title="Min profil" size="sm" backgroundColor="" />
-            <LogoutButton title="Logga ut" size="sm" backgroundColor="" />
-            </View>
-        </View>
+
     )
-        const showMeny = true
+
+    
     return (
+
         <View style={styles.body}>
             <View style={styles.headerContainer}>
                 <View style={styles.header}>
-                  {showMeny && <Meny ></Meny>}
-                  
                     <Image
                         style={styles.logo}
                         source={require('../assets/logo.png')}
                     />
-                      <TouchableOpacity >
-                    <Image
-                        style={styles.icon}
-                        source={require('../assets/BuilderIcon.png')}
-                    />
+                    <TouchableOpacity onPress={Meny}>
+                        <Image
+                            style={styles.icon}
+                            source={require('../assets/BuilderIcon.png')}
+                        />
                     </TouchableOpacity>
                 </View>
             </View>
             <View>
+
                 <AppButton title="Skapa ett ärende" size="sm" backgroundColor="" />
             </View>
+
             <Text style={styles.user}>
                 Dabstra
             </Text>
             <View style={styles.textContainer}>
-            <Text style={styles.text}>
-            Philip ska bjuda Dabstra på öl 🍺    
+                <Text style={styles.text}>
+                    Philip ska bjuda Dabstra på öl 🍺
             </Text>
             </View>
             <View style={styles.textContainer}>
@@ -84,7 +92,17 @@ export default function ScreenA({ navigation }) {
             <View style={styles.textContainer}>
 
             </View>
-
+            {showMeny && <View style={styles.meny}>
+                <View style={styles.triangle}>
+                </View>
+                    <TouchableOpacity style={styles.exitMeny} onPress={Back}>
+                        <Text style={styles.exitMenyText}>X</Text>
+                    </TouchableOpacity>
+                <View>
+                    <ProfileButton title="Min profil" size="sm" backgroundColor="" />
+                    <LogoutButton title="Logga ut" size="sm" backgroundColor="" />
+                </View>
+            </View>}
         </View>
     )
 }
@@ -106,14 +124,12 @@ const styles = StyleSheet.create({
         flexDirection: 'row',
         justifyContent: 'space-around',
         alignItems: 'center',
-        elevation: 8,
         backgroundColor: "#FDDF75",
         width: 280,
         height: 50,
         borderRadius: 50,
         margin: 30,
         padding: 20,
-    
     },
     appButtonText: {
         fontSize: 24,
@@ -132,42 +148,83 @@ const styles = StyleSheet.create({
     icon: {
         margin: 34
     },
-    text:{
+    text: {
         padding: 8
     },
-    user:{
+    user: {
         alignSelf: 'flex-start',
         marginLeft: 30
 
     },
-    meny:{
+    meny: {
         position: 'absolute',
-        top: 650,
-        left: 150,
-        width: 180,
-        height: 100,
+        top: 84,
+        left: 180,
+        width: 200,
+        height: 120,
         backgroundColor: '#D7D7D7',
         borderRadius: 10,
-        
+
+
     },
-    menybuttonContainer:{
+    ProfileButtonContainer: {
+        position: 'relative',
         flexDirection: 'row',
         justifyContent: 'space-around',
         alignItems: 'center',
         backgroundColor: "#FDDF75",
+        top: 20,
         borderRadius: 50,
         height: 30,
-        margin: 10,
         paddingLeft: 25,
         paddingRight: 25,
+        marginLeft: 20,
+        marginRight: 20,
+    },
+    LogoutButtonContainer: {
+        position: 'relative',
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        alignItems: 'center',
+        backgroundColor: "#FDDF75",
+        top: 40,
+        borderRadius: 50,
+        height: 30,
+        paddingLeft: 25,
+        paddingRight: 25,
+        marginLeft: 20,
+        marginRight: 20,
     },
     menybuttonText: {
         fontSize: 15,
         color: "#000000",
     },
-    menybuttonLogo:{
-       width: 15,
-       height: 20, 
+    menybuttonLogo: {
+        width: 15,
+        height: 20,
+    },
+    triangle: {
+        position: 'absolute',
+        top: -18,
+        left: 160,
+        width: 20,
+        height: 20,
+        backgroundColor: "transparent",
+        borderLeftWidth: 10,
+        borderRightWidth: 10,
+        borderBottomWidth: 20,
+        borderLeftColor: "transparent",
+        borderRightColor: "transparent",
+        borderBottomColor: "#D7D7D7",
+
+    },
+    exitMeny: {
+        position: 'absolute',
+        left: 180,
+    },
+    exitMenyText:{
+        fontSize:18
     }
+
 
 });
