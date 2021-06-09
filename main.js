e48e4bfef767b01f462d4a3d65467e2c1b18b0ad
rutbergphilip <philiprutberg00@gmail.com>
2021-06-09 15:07:52 +0200

    base code for api

--- main.js
+++ main.js
@@ -1,5 +1,16 @@
 import registerRootComponent from 'expo/build/launch/registerRootComponent';
 
 import App from './src/App';
+import axios from 'axios'
 
 registerRootComponent(App);
+
+const HOST = '10.0.2.2'
+const API = azios.create({
+    baseUrl: `https://${HOST}:5000/api/v1`
+})
+
+export async function login() {
+    const response = await API.post('/authenticate')
+    if (response.code === 200) return response.data
+}
